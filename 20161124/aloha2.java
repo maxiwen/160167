aad77564b2823f49284d5696f3abb3c5a0863f79
maxiwen <maxiwen521@126.com>
2016-11-24 11:35:06 +0900

    aloha2の練習

---
+++ 20161124/aloha2.java
@@ -0,0 +1,11 @@
+public class aloha2{
+	public static void main(String[] args) {
+		for(int i=0; i<30; i++){
+			if(i == 10 || i == 20){
+				System.out.println("アロハ");
+			} else {
+				System.out.println("aloha");
+			}
+		}
+	}
+}
