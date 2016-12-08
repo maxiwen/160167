ea57edac3eda9698355a5ff72429b6c541dc9a3c
maxiwen <maxiwen521@126.com>
2016-12-08 09:57:06 +0900

    1

---
+++ 20161129/aloha1.java
@@ -0,0 +1,5 @@
+public class aloha1{
+	public static void main(String[] args) {
+		System.out.println(args[0]+"さん、アロハ！");
+	}
+}
---
+++ 20161208/Aloha.java
@@ -0,0 +1,12 @@
+public class Aloha{
+	public static void main(String[] args) {
+		//for (int i=0; i<30; i++) {
+			//System.out.println("Aloha");
+		int i=0;
+		while(i<30){
+			System.out.println("Aloha");
+			i++;
+		
+		}
+	}
+}
