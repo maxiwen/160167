1f0f28491fffdf7fba4570f6b7e6aea381514593
maxiwen <maxiwen521@126.com>
2016-12-13 09:45:03 +0900

    4

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -3,10 +3,13 @@ public class Aloha{
 
 			System.out.print("Input data:");
 			String data = new java.util.Scanner(System.in).nextLine();
-			System.out.println(data);		
+			System.out.println(data);	
+
+				
 
 			if(data.equals("exit")){
 				System.out.println("終わります");
 			}
 		}
 	}
+　
