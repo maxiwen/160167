d9a2f22e69e713372fffa454ec053d71eaa844a3
maxiwen <maxiwen521@126.com>
2016-12-22 09:55:38 +0900

    MainFrameMan.java added.

---
+++ 20161222/MainFrameMan.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MainFrameMan{
+	public static void main(String[] args) {
+		FrameMan fm = new FrameMan();
+
+	}
+}
+
+class FrameMan{
+	JFrame frame;
+
+	public FrameMan(){
+		System.out.println("Aloha2");
+	}
+}
